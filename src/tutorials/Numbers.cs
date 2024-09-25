4f3a5a108810926e25fa6eb085fc9ceacab5cf10
Samul1 <juutinen.samuli@gmail.com>
2024-09-25 21:26:05 +0300

    added new tutorials: Strings.cs and Numbers.cs

---
+++ src/tutorials/Numbers.cs
@@ -0,0 +1,118 @@
+﻿int a = 18;
+int b = 6;
+int c = 8;
+int d = 5;
+int e = 4;
+int f = 2;
+int g = 1;
+int h = 7;
+int i = 4;
+int j = 3;
+
+double[] numDoubles = [5, 4, 2, 19, 23, 8];
+
+// ------------------------- Basic integer math -----------------------------
+Console.WriteLine("--------- Basic integer math -----------");
+
+// Plus
+int result1 = a + b;
+Console.WriteLine($"Plus: {result1}");
+// Minus
+result1 = a - b;
+Console.WriteLine($"Minus: {result1}");
+// Multiplication
+result1 = a * b;
+Console.WriteLine($"Multiplication: {result1}");
+// Division
+result1 = a / b;
+Console.WriteLine($"Division: {result1}");
+Console.WriteLine();
+
+
+// C# is smart and can use correct order of operations
+int result2 = d + e * f;
+Console.WriteLine($"Correct order of operations: {result2}");
+
+
+// You can force a different order of operations
+result2 = (d + e) * f;
+Console.WriteLine($"Forced order of operations: {result2}");
+
+
+// Because of integer type, there will be no decimals
+int result3 = (h + i) / j;
+Console.WriteLine($"No decimals: {result3}");
+Console.WriteLine();
+
+// ----------------- ingerer precision and limits --------------------------
+
+// You can get the remainder by using the remainder operator, the % character:
+int result4 = (h + i) / j;
+int result5 = (h + i) % j;
+Console.WriteLine($"Quotient: {result4}");
+Console.WriteLine($"Remainder: {result5}");
+Console.WriteLine();
+
+// Min Max limits of integer type
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+// If a calculation produces a value that exceeds those limits, you have an underflow or overflow condition. 
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+Console.WriteLine();
+
+// ----------------------------- Work with the double ----------------------
+
+Console.WriteLine("------------- Work with the double ----------------------");
+Console.WriteLine();
+// Lets see some decimals
+double result6 = (numDoubles[0] + numDoubles[1]) / numDoubles[2];   // (5+4)/2 = 4.5
+Console.WriteLine(result6);
+
+double result7 = (numDoubles[3] - numDoubles[4]) / numDoubles[5];   // (19+23)/8 = -0.5
+Console.WriteLine(result7);
+
+double maxDouble = double.MaxValue;
+double minDouble = double.MinValue;
+Console.WriteLine($"The range of double is {minDouble} to {maxDouble}");
+
+// Just like decimal numbers in math, doubles in C# can have rounding errors.
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+Console.WriteLine();
+
+// ----------------------------- Work with decimal --------------------------
+Console.WriteLine("------------- Work with decimal ----------------------");
+Console.WriteLine();
+
+// The decimal type has a smaller range but greater precision than double.
+
+decimal minDecimal = decimal.MinValue;
+decimal maxDecimal = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {minDecimal} to {maxDecimal}");
+
+// Notice that the range is smaller than the double type.
+// You can see the greater precision with the decimal type by trying the following code:
+double do1 = 1.0;
+double do2 = 3.0;
+Console.WriteLine(do1 / do2);
+
+decimal de1 = 1.0M;
+decimal de2 = 3.0M;
+Console.WriteLine(de1 / de2);
+Console.WriteLine();
+
+// Write code that calculates the area of a circle whose radius is 2.50 centimeters.
+// Remember that the area of a circle is the radius squared multiplied by PI
+
+double radius = 2.50;
+double area = Math.PI * radius * radius; // PI * r^2
+Console.WriteLine(area);
+Console.WriteLine();
+
+// Math.Abs(x) ----------------------------------------------------------------
+// The Math.Abs(x) method returns the absolute(positive) value of x:
+
+Console.WriteLine(Math.Abs(-4.7));         // Returns 4.7
---
+++ src/tutorials/Strings.cs
@@ -0,0 +1,73 @@
+﻿// Output a message to console
+Console.WriteLine("Hello world!");
+// Empty line in console window
+Console.WriteLine();
+// -------------------------------- Work With String ---------------------------------------
+
+Console.WriteLine("----------- Basic String Stuff: -------------------");
+Console.WriteLine();
+
+// String variable
+string[] strings = ["Juho", "Paula", "        Hello World        ", "3", "4", "5"];
+Console.WriteLine(strings[0]);
+
+// You can use string type variables in console printing
+Console.WriteLine("My friend is: " + strings[0]);
+// Can also be done like this
+Console.WriteLine($"My friend is: {strings[0]}");
+
+// You can also use multible string variables in sentence.
+Console.WriteLine($"My friend is {strings[0]} and my fiance is {strings[1]}.");
+
+// You can see what is string length
+Console.WriteLine($"Name '{strings[1]}' has {strings[1].Length} letters.");
+
+// --------------------------------- Upper and Lower cases -----------------------------
+
+Console.WriteLine();
+Console.WriteLine("------------- Upper and Lower cases -----------------");
+Console.WriteLine();
+
+// You also can put all letter to lower or upper cases
+Console.WriteLine($"{strings[1]} {strings[1].ToLower()} {strings[1].ToUpper()}");
+Console.WriteLine();
+
+// --------------------------------- Trim:ing -----------------------------------------
+Console.WriteLine("------------------ Trimming: --------------------- ");
+Console.WriteLine();
+
+// Too many spaces
+Console.WriteLine($"{strings[2]}");
+
+// So lets trim empty spaces
+Console.WriteLine($"[{strings[2].TrimStart()}]");
+Console.WriteLine($"[{strings[2].TrimEnd()}]");
+Console.WriteLine($"[{strings[2].Trim()}]");
+Console.WriteLine();
+
+// -------------------------------- Replace -------------------------------------------
+
+Console.WriteLine(" ----------------- Replace: -------------------------");
+
+// This method takes too variables and replaces first with second
+strings[3] = "Hello there!";
+Console.WriteLine(strings[3]);
+strings[3] = strings[3].Replace("Hello there!", "Greetings!");
+Console.WriteLine(strings[3]);
+Console.WriteLine();
+
+// ------------------------------- Search string ---------------------------------------
+
+Console.WriteLine("-------------- Search string -------------------------");
+
+strings[4] = "Say something I'm giving up on you";
+// .Contains return true or false if string contains a substring inside it
+Console.WriteLine(strings[4].Contains("giving"));   // True
+Console.WriteLine(strings[4].Contains("meet"));     // False
+
+// .StartsWith search if the string start with searchword
+Console.WriteLine(strings[4].StartsWith("Say"));    // True
+Console.WriteLine(strings[4].StartsWith("They"));   // False
+// .EndsWith search if the string end with searchword
+Console.WriteLine(strings[4].EndsWith("you"));      // True
+Console.WriteLine(strings[4].EndsWith("me"));       // False
